59e9281d94acf529113697057d80bb6a1eac6191
Remi Louf <remilouf@gmail.com>
2015-02-19 11:14:03 +0100

    Add global init file

---
+++ __init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""Marble: analyse social stratification in cities"""
+
+
+__author__ = "Rémi Louf"
+__email__ = "remilouf@sciti.es"
+__website__ = "www.sciti.es"
+__copyright__ = "2015, Rémi Louf"
