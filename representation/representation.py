be94d193d4954dd6ed3762716e5ea83750efd6f8
Remi Louf <remilouf@gmail.com>
2015-02-20 21:37:11 +0100

    Write the header for the representation functions.

---
+++ representation/representation.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""representation.py
+
+Compute the representation of the different populations in the areal units
+provided as an input.
+
+See [Louf:2015]_ for the definition of the different quantities
+"""
+
+__all__ = ['representation']
+
+
+#
+# Helper functions
+#
+
+
+
+#
+# Callable functions
+#
+def representation(distribution, classes=None):
+    representation = 
+
