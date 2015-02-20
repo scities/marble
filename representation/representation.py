55629015b5ba1b9485daca72e274bf728009faff
Remi Louf <remilouf@gmail.com>
2015-02-21 00:52:09 +0100

    Corrected presentation, function to regroup categories by class, or output classes as encoded in the data

--- representation/representation.py
+++ representation/representation.py
@@ -25,29 +25,58 @@ def compute_totals(distribution, classes):
 
 
 def single_representation(n, n_unit, N_class, N_tot):
+    "Compute the representation of a population in a given areal unit"
     return (n/n_unit) / (N_class/N_tot)
 
 
 def single_deviation(n_unit, N_class, N_tot):
+    "Compute the standard deviation in a given areal unit"
     return math.sqrt( (1/N_class)*((N_tot/n_unit) - 1) ) 
+
+
+def regroup_per_class(distribution, classes):
+    "Return classes as they are presented in the data"
+    new_distribution =  {au: {cl: sum([dist_au[c] for c in composition]) 
+                              for cl,composition in classes.iteritems()}
+                         for au, dist_au in distribution.iteritems()}
+
+    return new_distribution
+
+
+def return_categories(distribution):
+    keys = next(distribution.itervalues()).keys()
+    return {k:[k] for k in keys}
+
+
+
 
 
 #
 # Callable functions
 #
 def representation(distribution, classes=None):
-  
+   
+    # Regroup into classes if specified. Otherwise return categories indicated
+    # in the data
+    if classes:
+        distribution = regroup_per_class(distribution, classes)
+    else:
+       classes = return_categories(distribution) 
+
+
     # Compute the total numbers per class and per individual
     N_unit, N_class, N_tot = compute_totals(distribution) 
 
+
+    # Compute the representation and standard deviation for all areal units
     representation = {au:{cl:(single_representation(dist_au[cl],
                                                     N_unit[au],
                                                     N_class[cl],
                                                     N_tot), 
                               single_deviation(N_unit[au],
                                                N_class[cl],
-                                               N_tot) )
-                          for cl in classes}
+                                               N_tot) 
+                             ) for cl in classes}
                       for au, dist_au in distribution.iteritems()}
     
     return representation
