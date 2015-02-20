fc55c50c2c88e096ed519b1bcccf87b2916b5545
Remi Louf <remilouf@gmail.com>
2015-02-20 21:52:18 +0100

    Mock functions to compute representation and standard deviation. Function to compute the representation writtten.

--- representation/representation.py
+++ representation/representation.py
@@ -20,16 +20,32 @@ def compute_totals(distribution, classes):
     N_tot = sum(N_class,values())
     return N_unit, N_class, N_tot
 
-def single_representation()
+
+def single_representation(n, n_unit, N_class, N_tot):
+    pass
+
+
+def single_deviation(n, n_unit, N_class, N_tot):
+    pass
+
 
 #
 # Callable functions
 #
 def representation(distribution, classes=None):
-    representation = 
   
     # Compute the total numbers per class and per individual
     N_unit, N_class, N_tot = compute_totals(distribution) 
 
-    for areal_unit in distribution:
-        rep = single_representation(distribution[areal_unit][cl], 
+    representation = {au:{cl:(single_representation(dist_au[cl],
+                                                    N_unit[au],
+                                                    N_class[cl],
+                                                    N_tot), 
+                              single_deviation(dist_au[cl],
+                                               N_unit[au],
+                                               N_class[cl],
+                                               N_tot) )
+                          for cl in classes}
+                      for au, dist_au in distribution.iteritems()}
+    
+    return representation
