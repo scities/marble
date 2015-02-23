5a47c7e7cf3712d1a1d98d41297051c698e691c0
Remi Louf <remilouf@gmail.com>
2015-02-23 12:15:03 +0100

    Add functions to compute the exposure and the associated variance

--- exposure/exposure.py
+++ exposure/exposure.py
@@ -26,12 +26,40 @@ __all__ = ["exposure"]
 #
 # Helper functions
 #
-def single_exposure(distribution, cl0, cl1):
-    pass
 
-def single_deviation(distribution, cl0, cl1):
-    pass
+## To compute the exposure
+def pair_exposure(r, N_unit, N_tot, alpha, beta):
+    E = (1/N_tot)*sum([N_unit[au]*r[au][alpha]*r[au][beta] for au in r] 
+    return E
 
+
+
+## To compute the variance of the exposure
+def unit_variance(N_tot, N_t, N_alpha, N_beta):
+    "Compute the variance of E_{\alpha \beta}(t)" 
+    var = (1/(N_alpha*N_beta)) * ((N_tot/N_t)-1)**2 \
+            + (1/N_alpha) * ((N_tot/N_t)-1) \
+            + (1/N_beta) * ((N_tot/N_t)-1)
+
+    return var
+
+
+def units_covariance(N_alpha, N_beta):
+    "Compute the covariance of E_{\alpha \beta}(s) and E_{\alpha \beta}(t)"
+    return (1-(1/N_alpha)) + (1-(1/N_beta)) -1 
+
+
+def pair_variance(r, N_unit, N_class, N_tot, alpha, beta):
+    var = (1/N_tot**2) * ( sum([(N_unit[au]**2)*unit_variance(N_tot, 
+                                                              N_unit[au],
+                                                              N_class[alpha], 
+                                                              N_class[beta]) 
+                                for au in r]) 
+        + 2*sum([N_unit[au0]*N_unit[au1]*units_covariance(N_class[alpha],
+                                                          N_class[beta])
+                for au0, au1 in itertools.combination_with_replacement(r, 2)]) )
+           
+            
 
 
 
@@ -54,9 +82,9 @@ def exposure(distribution, classes):
     representation = rep(distribution, classes)
 
     # Compute the exposure matrix
-    exposure = {cl0: {cl1: (single_exposure(distribution, cl0, cl1),
-                            single_deviation(distribution, cl0, cl1)) }
-                for cl0, cl1 
+    exposure = {alpha: {beta: (pair_exposure(representation, N_unit, N_tot, alpha, beta),
+                               pair_variance(representation, N_class, N_unit, N_tot, alpha, beta)) }
+                for alpha, beta 
                 in itertools.combination_with_replacement(classes,2)} 
 
     return exposure 
