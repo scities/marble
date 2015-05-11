08ce08ad13559602464f238c50615b4e9bc26769
Remi Louf <remilouf@gmail.com>
2015-05-11 14:30:02 +0100

    Add setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='marble',
+    version='0.1',
+    description='A python script to study residential segregation',
+    url='http://github.com/scities/marble',
+    author='Rémi Louf',
+    author_email='remi.louf@sciti.es',
+    license='BSD',
+    packages=['marble'],
+    zip_safe=False)
