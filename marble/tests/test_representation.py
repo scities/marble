f67168fbc42152b5d77933a6a1395c3cd7d94ce5
Remi Louf <remilouf@gmail.com>
2015-05-21 20:17:02 +0100

    List of tests to implement

---
+++ marble/tests/test_representation.py
@@ -0,0 +1,9 @@
+""" Tests for the representation computation """
+from nose.tools import *
+import marble as mb
+
+
+# Test r=0 where there is no one
+# Test pre-computed representation values and variance
+# Test NaN when no one from a given class
+# Test NaN when no one in an areal unit
