730d49facec1c1f288a25f505eee2d2b671891a9
Remi Louf <remilouf@gmail.com>
2015-05-21 21:06:22 +0100

    Fill in informations in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from setuptools import setup
 
-setup(name='marble',
+def readme():
+    with open('README.rst.example') as f:
+        return f.read()
+
+(name='marble',
     version='0.1',
-    description='A python script to study residential segregation',
+    description=('A python library to study socio-spatial stratification '+
+                 'in cities'),
+    long_description=readme(),
     url='http://github.com/scities/marble',
     author='Rémi Louf',
     author_email='remi.louf@sciti.es',
     license='BSD',
     packages=['marble'],
+    setup_requires=[
+        'networkx',
+        'shapely'
+        ],
+    testt_suite='nose.collector',
+        tests_require=['nose'],
     zip_safe=False)
