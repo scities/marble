552437791d8efa527f139716d1ede2d7262c97f1
Remi Louf <remilouf@gmail.com>
2015-05-26 20:12:39 +0100

    Test the class agregation

--- marble/tests/test_classes.py
+++ marble/tests/test_classes.py
@@ -3,5 +3,39 @@ from nose.tools import *
 import marble as mb
 
 
-# Test that under uniform repartition, only one class
-# Test extreme separation
+
+#
+# Synthetic data for the tests
+#
+def uniform_city():
+    """ 2-way repartition """
+    city = {"A":{1:10, 2:10, 3:0},
+            "B":{1:20, 2:20, 3:0},
+            "C":{1:0, 2:0, 3:40}}
+    return city
+
+def segregated_city():
+    """ 2-way segregation """
+    city = {"A":{1:3107, 2:0, 3:0},
+            "B":{1:0, 2:0, 3:1184},
+            "C":{1:0, 2:1242, 3:0}}
+    return city
+
+
+
+#
+# Tests
+#
+class TestClasses(object):
+
+    def test_class_uniform(self):
+        city = uniform_city()
+        exp = mb.exposure(city)
+        classes = mb.uncover_classes(city, exp)
+        assert_equal(len(classes),2)
+
+    def test_class_segregated(self):
+        city = segregated_city()
+        exp = mb.exposure(city)
+        classes = mb.uncover_classes(city, exp)
+        assert_equal(len(classes),3)
