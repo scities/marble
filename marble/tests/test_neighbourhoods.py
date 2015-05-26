73fb4797e21f5c1ed3330d538a81cadae31afe4c
Remi Louf <remilouf@gmail.com>
2015-05-26 18:46:16 +0100

    Finish to write tests of the neighbourhoods extraction

--- marble/tests/test_neighbourhoods.py
+++ marble/tests/test_neighbourhoods.py
@@ -11,6 +11,7 @@ from marble.neighbourhoods import _adjacency
 # Synthetic data for tests
 #
 def grid():
+    """ Areal units arranged in a grid """
     au = [i*3+j for i,j  in itertools.product(range(3), repeat=2)]
     units = {a:Polygon([(a%3, a/3),
                         (a%3, 1+a/3),
@@ -18,8 +19,35 @@ def grid():
                         (1+a%3, a/3)]) for a in au}
     return units
 
+def checkerboard_city():
+    city = {0: {"A":100, "B":1},
+            1: {"A":1, "B":100},
+            2: {"A":100, "B":1},
+            3: {"A":1, "B":100},
+            4: {"A":100, "B":1},
+            5: {"A":1, "B":100},
+            6: {"A":100, "B":1},
+            7: {"A":1, "B":100},
+            8: {"A":100, "B":1}}
+    return city
+
+def clustered_city():
+    city = {0: {"A":100, "B":1},
+            1: {"A":100, "B":1},
+            2: {"A":1, "B":100},
+            3: {"A":100, "B":1},
+            4: {"A":1, "B":100},
+            5: {"A":1, "B":100},
+            6: {"A":100, "B":1},
+            7: {"A":1, "B":100},
+            8: {"A":1, "B":100}}
+    return city
 
 
+
+#
+# Perform tests
+#
 class TestNeighbourhoods(object):
 
     def test_adjacency(test):
@@ -39,6 +67,44 @@ class TestNeighbourhoods(object):
         for au in adj:
             assert set(adj[au]) == set(adj_answer[au])
 
-# Test that for a grid, corners are not neighbours (.touch might have to go)
-# Test clustering on a situation
+    def test_overrepresented_check(self):
+        """ Test the extraction of units with overrepresentation """
+        city = checkerboard_city()
+        units = mb.overrepresented_units(city)
+        units_answer = {"A":[0,2,4,6,8],
+                        "B":[1,3,5,7]}
 
+        assert set(units["A"]) == set(units_answer["A"])
+        assert set(units["B"]) == set(units_answer["B"])
+
+    def test_overrepresented_clust(self):
+        """ Test the extraction of units with overrepresentation """
+        city = clustered_city()
+        units = mb.overrepresented_units(city)
+        units_answer = {"A":[0,1,3,6],
+                        "B":[2,4,5,7,8]}
+
+        assert set(units["A"]) == set(units_answer["A"])
+        assert set(units["B"]) == set(units_answer["B"])
+
+    def test_neighbourhoods_check(self):
+        """ Test the extraction of neighbourhoods """
+        city = checkerboard_city()
+        units = grid()
+        neigh = mb.neighbourhoods(city, units)
+        neigh_answer = {"A":[[0],[2],[4],[6],[8]],
+                        "B":[[1],[3],[5],[7]]}
+
+        assert len(neigh["A"]) == len(neigh_answer["A"])
+        assert len(neigh["B"]) == len(neigh_answer["B"])
+
+    def test_neighbourhoods_clust(self):
+        """ Test the extraction of neighbourhoods """
+        city = clustered_city()
+        units = grid()
+        neigh = mb.neighbourhoods(city, units)
+        neigh_answer = {"A":[[0,1,3,6]],
+                        "B":[[2,4,5,7,8]]}
+
+        assert len(neigh["A"]) == len(neigh_answer["A"])
+        assert len(neigh["B"]) == len(neigh_answer["B"])
